ecc75ee5fc4c54767231020d85271fe8c003ebd9
ProqsiEugene <proqsi43@gmail.com>
2023-05-04 16:47:36 +0500

    work3

--- src/main/java/com/example/testcamel/route/TrainRoutePost.java
+++ src/main/java/com/example/testcamel/route/TrainRoutePost.java
@@ -2,17 +2,18 @@ package com.example.testcamel.route;
 
 import com.example.testcamel.dto.DateDTO;
 import com.example.testcamel.dto.TrainDTO;
-import com.example.testcamel.service.MyProcessor;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalTime;
+import java.util.UUID;
 
 @Component(value = "trainRoute")
 public class TrainRoutePost extends RouteBuilder {
-
-    @Autowired
-    private MyProcessor myProcessor;
 
     @Override
     public void configure() throws Exception {
@@ -25,23 +26,30 @@ public class TrainRoutePost extends RouteBuilder {
                 .to("direct:convertToTrain");
 
         from("direct:convertToTrain")
+                .routeId("convertToTrain")
                 .log("Что попадает в конвектор: " + "${body}")
                 .process(exchange -> {
+                    ServletRequest request = exchange.getIn().getBody(HttpServletRequest.class);
                     TrainDTO trainDTO = new TrainDTO();
                     trainDTO.setDate(exchange.getIn().getBody(DateDTO.class).getDate().toString());
+                    trainDTO.setTime(LocalTime.now().toString());
+                    trainDTO.setIp(request.getRemoteAddr());
 
+
+                 //   trainDTO.setIp(exchange.getIn().getHeader(Exchange.HTTP_HOST, String.class));
+
+                    trainDTO.setGuid(UUID.randomUUID().toString());
                     exchange.getIn().setBody(trainDTO);
                 })
+                .setProperty("bodyValue", body()) // сохраняю body в переменную bodyValue
                 .log("Что выходит из конвектора: " + "${body}")
                 .to("direct:body");
 
         from("direct:body")
                 .routeId("bodyMessage")
-                .log("Что попадает в процессор: " + "${body}")
-                .process(myProcessor)
-                .setProperty("bodyValue", body()) // сохраняю body в переменную bodyValue
-                .log("Что выходит из процессора: " + "${body}")
+                .log("Что попадает в bodyMessage: " + "${body}")
                 .to("bean:protobufService?method=convertDtoToProtobuf")
+                .log("Что выходит из bodyMessage после протобаф: " + "${body}")
                 .to("direct:kafka");
 
         from("direct:kafka")
@@ -57,17 +65,16 @@ public class TrainRoutePost extends RouteBuilder {
                 .setBody(simple("${exchangeProperty.bodyValue}"))
                 .log("После метода setBody в sendDtoToDB: " + "${body}")
                 .log("${body.getTime}")
-
-
                 .to("sql:INSERT INTO sessions (time_session, ip_session, date_session, guid_session) " +
                         "values (:#${body.getTime}, :#${body.getIp}, :#${body.getDate}, :#${body.getGuid})")
                 .to("log:output")
                 .to("direct:fromDtoToDB");
 
         from("direct:fromDtoToDB")
+                .routeId("fromDtoToDB")
                 .log("До  fromDtoToDB: " + "${body}")
                 .setBody(simple("${exchangeProperty.bodyValue.getGuid}")) //достаю тело из переменной bodyValue и сохр в body
                 .log("guid: " + "${body}")
                 .to("log:output");
     }
-    }
+}
